c47adfcf69d1debbc4d9d1943e53dfada9d6823b
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2020-08-18 21:49:03 +0300

    🔨  Split leaflet to sepparate chunk

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,5 +2,22 @@ module.exports = {
   // Avoid the CORS error from vue-cli
   devServer: {
     proxy: 'http://backend.test/'
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        optimization: {
+          splitChunks: {
+            cacheGroups: {
+              vendor: {
+                test: /[\\/]node_modules[\\/](leaflet)[\\/]/,
+                name: 'leaflet',
+                chunks: 'all',
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
